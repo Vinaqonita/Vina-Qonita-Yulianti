745a381633004ebfa235fefeeb6a5baa783bdcff
Vinaqonita <114374496+Vinaqonita@users.noreply.github.com>
2022-09-26 10:01:03 +0700

    Add files via upload

---
+++ Tugas OOP Inheritance.php
@@ -0,0 +1,89 @@
+<?php
+
+    class produc
+    {
+        public $name;
+        public $price;
+        public $discount
+        public function __construct($name, $color, $discount) {
+            $this->name = $name;
+            $this->color = $color;
+            $this->discount = $discount;
+        }
+    }
+    {
+        function_construct()
+        {
+
+        }
+        function getPrice()
+        {
+            echo"";
+        }
+        function setPrice()
+        {
+            echo"";
+        }
+        function getName()
+        {
+            echo"";
+        }
+        function setName()
+        {
+            echo"";
+        }
+        function getDiscount()
+        {
+            echo"";
+        }
+        function setDiscount()
+        {
+            echo"";
+        }
+    }
+    class CDMusic extends produc
+    { 
+        protected $artist;
+        protected $genre;
+
+        function getArtist()
+        {
+            echo "";
+        }
+        function setArtist()
+        {
+            echo "";
+        }
+        function getGendre()
+        {
+            echo "";
+        }
+        function setGendre()
+        {
+            echo "";
+        }
+
+    }
+    class CDCabinet extends produc
+    {
+        protected $capacity;
+        protected $model;
+
+        function getCapacity()
+        {
+            echo "";
+        }
+        function setCapacity()
+        {
+            echo "";
+        }
+        function getModel()
+        {
+            echo "";
+        }
+        function setModel()
+        {
+            echo "";
+        }
+    }
+   
